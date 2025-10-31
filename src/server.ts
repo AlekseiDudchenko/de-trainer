5bdd135c2ee653bacbf6a8d07e96107daac5038f
Aleksei Dudchenko <pv.dudchenko@gmail.com>
2025-10-31 16:01:55 +0100

    Add logging for API endpoints

--- src/server.ts
+++ src/server.ts
@@ -27,28 +27,28 @@ app.get("/api/gaps/one", (_req: Request, res: Response) => {
   const gaps = readGaps();
   const gap = gaps[Math.floor(Math.random() * gaps.length)];
   res.json(gap); // { id, sentence, answer }
-
-
   console.log("call to /api/gaps/one. Gap:", gap);
-
 });
 
 app.get("/api/words", (_req: Request, res: Response) => {
   const filePath = path.join(__dirname, "..", "data", "words.json");
   const raw = fs.readFileSync(filePath, "utf8");
   res.json(JSON.parse(raw));
+  console.log("call to /api/words");
 });
 
 app.get("/api/sentences", (_req: Request, res: Response) => {
   const filePath = path.join(__dirname, "..", "data", "sentences.json");
   const raw = fs.readFileSync(filePath, "utf8");
   res.json(JSON.parse(raw));
+  console.log("call to /api/sentences");
 });
 
 app.get("/api/gaps", (_req: Request, res: Response) => {
   const filePath = path.join(__dirname, "..", "data", "gaps.json");
   const raw = fs.readFileSync(filePath, "utf8");
   res.json(JSON.parse(raw));
+  console.log("call to /api/gaps");
 });
 
 app.listen(PORT, () => {
